665a07c9d78a9b0147ed71dd70b9f3f255691566
bendover111222333444 <sigmasigmaonthewallwhoisthe2@gmail.com>
2026-06-02 16:36:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import { server as wisp } from "@mercuryworkshop/wisp-js/server";
-import { createServer } from "node:http";
 
-const server = createServer();
-server.on("upgrade", (req, socket, head) => {
-    wisp.routeRequest(req, socket, head);
-});
-
-server.listen(process.env.PORT || 8080);
+export default {
+  async fetch(request, env, ctx) {
+    if (request.headers.get("Upgrade") === "websocket") {
+      return wisp.handleRequest(request);
+    }
+    return new Response("Wisp Server", { status: 200 });
+  }
+};
